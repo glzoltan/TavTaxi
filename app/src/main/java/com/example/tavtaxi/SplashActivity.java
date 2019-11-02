2408cae40bdd650584391e5d769e53c09690bb13
emese789 <mezei.emese98@gmail.com>
2019-11-03 00:12:12 +0200

    Splash Screen Ready

---
+++ app/src/main/java/com/example/tavtaxi/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.tavtaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(3000)
+                .withBackgroundResource(R.drawable.background)
+                .withLogo(R.drawable.taxi)
+                .withAfterLogoText("TavTaxi");
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextSize(36);
+
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+
+    }
+}
