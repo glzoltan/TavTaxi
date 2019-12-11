b0e0672682dfc6a10858ee495715911dc8755d0b
emese789 <mezei.emese98@gmail.com>
2019-12-11 13:42:20 +0200

    Creat call button functionality

--- app/src/main/java/com/example/tavtaxi/ListTravels.java
+++ app/src/main/java/com/example/tavtaxi/ListTravels.java
@@ -1,11 +1,20 @@
 package com.example.tavtaxi;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,37 +28,36 @@ import java.util.List;
 public class ListTravels extends AppCompatActivity {
     RecyclerView recyclerView;
     TripsAdapter adapter;
+    TextView etNumber;
     private DatabaseReference mDatabase;
 
     List<Fire_Trip> tripList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_travels);
-
+        etNumber = findViewById(R.id.textPhone);
         tripList = new ArrayList<>();
 
         recyclerView = findViewById(R.id.recyclerViewTrips);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-       // Fire_Trip newTrip = new Fire_Trip("id","Here","There","When", "Freestates",null);
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
-       // mDatabase.child("trips").push().setValue(newTrip);
         mDatabase.child("trips").orderByKey().addValueEventListener(
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         // for example: if you're expecting your user's data as an object of the "User" class.
-                        for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
                             Fire_Trip trips = ds.getValue(Fire_Trip.class);
                             String from = trips.getFrom();
                             String where = trips.getWhere();
                             String when = trips.getWhen();
                             String freestates = trips.getFreeStates();
-                            String phone=trips.getPhoneNumber();
-                            ArrayList<String> cities=trips.getCities();
-                            tripList.add(new Fire_Trip("id", from, where, when,freestates,cities,phone));
+                            String phone = trips.getPhoneNumber();
+                            ArrayList<String> cities = trips.getCities();
+                            tripList.add(new Fire_Trip("id", from, where, when, freestates, cities, phone));
                             adapter = new TripsAdapter(ListTravels.this, tripList);
                             recyclerView.setAdapter(adapter);
                         }
@@ -60,5 +68,6 @@ public class ListTravels extends AppCompatActivity {
                         // read query is cancelled.
                     }
                 });
+
     }
 }
--- app/src/main/java/com/example/tavtaxi/TripsAdapter.java
+++ app/src/main/java/com/example/tavtaxi/TripsAdapter.java
@@ -1,19 +1,28 @@
 package com.example.tavtaxi;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TripsAdapter extends  RecyclerView.Adapter<TripsAdapter.TripsViewHolder> {
+public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.TripsViewHolder> {
 
     private Context Context;
     private List<Fire_Trip> TripList;
@@ -27,12 +36,12 @@ public class TripsAdapter extends  RecyclerView.Adapter<TripsAdapter.TripsViewHo
     @Override
     public TripsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(Context);
-        View view = inflater.inflate(R.layout.one_list_item,null);
+        View view = inflater.inflate(R.layout.one_list_item, null);
         return new TripsViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TripsViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final TripsViewHolder holder, int position) {
 
         Fire_Trip trip = TripList.get(position);
         holder.from.setText(trip.getFrom());
@@ -40,15 +49,28 @@ public class TripsAdapter extends  RecyclerView.Adapter<TripsAdapter.TripsViewHo
         holder.when.setText(trip.getWhen());
         holder.states.setText(trip.getFreeStates());
         holder.phone.setText(trip.getPhoneNumber());
-        ArrayList<String> cts=trip.getCities();
+        ArrayList<String> cts = trip.getCities();
         String item = "";
-        for(int i=0;i < cts.size();i++){
+        for (int i = 0; i < cts.size(); i++) {
             item = item + cts.get(i);
-            if(i != cts.size()-1){
-                item= item + ", ";
+            if (i != cts.size() - 1) {
+                item = item + ", ";
             }
         }
         holder.cities.setText(item);
+        holder.buttonCall.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(Context,Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    String phone = holder.phone.getText().toString();
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+                    Context.startActivity(intent);
+                    return;
+                }
+
+            }
+        });
 
     }
 
@@ -61,6 +83,7 @@ public class TripsAdapter extends  RecyclerView.Adapter<TripsAdapter.TripsViewHo
 
         TextView from,to,when,cities;
         TextView phone,states;
+        Button buttonCall;
         public TripsViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -70,6 +93,7 @@ public class TripsAdapter extends  RecyclerView.Adapter<TripsAdapter.TripsViewHo
             states =itemView.findViewById(R.id.free_tv);
             cities = itemView.findViewById(R.id.cities_tv);
             phone=itemView.findViewById(R.id.textPhone);
+            buttonCall=itemView.findViewById(R.id.call_btn);
 
         }
     }
